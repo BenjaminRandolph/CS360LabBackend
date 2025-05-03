b535ec4bfda67055273298a834798d9f75d85050
BenjaminRandolph <114035117+BenjaminRandolph@users.noreply.github.com>
2025-05-02 20:57:21 -0700

    Change database connection configuration

--- Lab E-Commerce Website API/Program.cs
+++ Lab E-Commerce Website API/Program.cs
@@ -4,7 +4,7 @@ using Lab_E_Commerce_Website_API;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var port = Environment.GetEnvironmentVariable("PORT") ?? "7096";
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8081";
 builder.WebHost.UseUrls($"http://*:{port}");
 
 var databaseURL = Environment.GetEnvironmentVariable("PGHOST");
@@ -17,7 +17,7 @@ var databasePassword = Environment.GetEnvironmentVariable("PGPASSWORD");
 
 builder.Services.AddControllers();
 //                                                        UseNpgsql(builder.Configuration.GetConnectionString("PostgresqlDatabase"))
-builder.Services.AddDbContext<DatabaseContext>(opt => opt.UseNpgsql("Host=turntable.proxy.rlwy.net:55279;Database=" + databaseName + ";Username=" + databaseUser + ";Password=" + databasePassword));
+builder.Services.AddDbContext<DatabaseContext>(opt => opt.UseNpgsql("Host=" + databaseURL + databasePort + ";Database=" + databaseName + ";Username=" + databaseUser + ";Password=" + databasePassword));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
